bd74a76d9be9f671f41c0f2772d74335b07e6b33
Wbekkers <Wbekkers@deloitte.nl>
2019-05-28 14:20:48 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,5 +1,22 @@
-function submitComment() {
-    console.log('Hello!')
-  }
-
+const submitComment = () => {
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    const commentSection = document.getElementById('comments')
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
   
+    comment.classList.add('comment')
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+  
+    comment.appendChild(h3)
+    comment.appendChild(p)
+  
+    commentSection.append(comment)
+  
+    inputField.value = null;
+    textArea.value = null;
+  }
